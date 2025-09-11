8846f04a43899398a5a06ff5eed884428f78e86b
francescoFH <francescofacca@gmail.com>
2025-09-11 22:57:16 +0100

    EF - Preparing the data model for EF Core

--- src/GameStore.Api/Features/Games/CreateGame/CreateGameEndpoint.cs
+++ src/GameStore.Api/Features/Games/CreateGame/CreateGameEndpoint.cs
@@ -23,6 +23,7 @@ public static class CreateGameEndpoint
             {
                 Name = gameDto.Name,
                 Genre = genre,
+                GenreId = genre.Id,
                 Price = gameDto.Price,
                 ReleaseDate = gameDto.ReleaseDate,
                 Description = gameDto.Description
