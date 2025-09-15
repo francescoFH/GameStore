583e65b8dbbbdc5e30bc57c4c5e4925757dc7b1d
francescoFH <francescofacca@gmail.com>
2025-09-15 22:44:36 +0100

    EF - Configuring the DBContext

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -4,6 +4,10 @@ using GameStore.Api.Features.Genres;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connString = "Data Source=GameStore.db";
+
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 builder.Services.AddTransient<GameDataLogger>();
 builder.Services.AddSingleton<GameStoreData>();
 
