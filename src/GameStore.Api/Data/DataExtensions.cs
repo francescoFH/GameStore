8c806cfe0e4f2c90af08157d2c6ca793abf6d760
francescoFH <francescofacca@gmail.com>
2025-09-17 23:05:47 +0100

    Working with Data - Data seeding

--- src/GameStore.Api/Data/DataExtensions.cs
+++ src/GameStore.Api/Data/DataExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using GameStore.Api.Models;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,11 +7,37 @@ namespace GameStore.Api.Data;
 
 public static class DataExtensions
 {
-    public static void MigrateDb(this WebApplication app)
+    public static void InitializeDB(this WebApplication app)
+    {
+        app.MigrateDb();
+        app.SeedDB();
+    }
+
+    private static void MigrateDb(this WebApplication app)
     {
         using var scope = app.Services.CreateScope();
         GameStoreContext dbContext = scope.ServiceProvider
                                           .GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    private static void SeedDB(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        GameStoreContext dbContext = scope.ServiceProvider
+                                          .GetRequiredService<GameStoreContext>();
+
+        if (!dbContext.Genres.Any())
+        {
+            dbContext.Genres.AddRange(
+                new Genre { Name = "Fighting" },
+                new Genre { Name = "Kids and Family" },
+                new Genre { Name = "Racing" },
+                new Genre { Name = "Roleplaying" },
+                new Genre { Name = "Sports" }
+            );
+
+            dbContext.SaveChanges();
+        }
     }
 }
