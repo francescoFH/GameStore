999599e0db28e821a8a05b07d42437c123f5f509
francescoFH <francescofacca@gmail.com>
2025-09-18 23:01:14 +0100

    Working with Data - Final queries and clean up

--- src/GameStore.Api/Features/Genres/GetGenres/GetGenresEndpoint.cs
+++ src/GameStore.Api/Features/Genres/GetGenres/GetGenresEndpoint.cs
@@ -1,15 +1,16 @@
 using GameStore.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Features.Genres.GetGenres;
 
 public static class GetGenresEndpoint
 {
-    public static void MapGetGenres(
-        this IEndpointRouteBuilder app)
+    public static void MapGetGenres(this IEndpointRouteBuilder app)
     {
         // GET /genres
-        app.MapGet("/", (GameStoreData data) =>
-            data.GetGenres()
-                .Select(genre => new GenreDto(genre.Id, genre.Name)));
+        app.MapGet("/", (GameStoreContext dbContext) =>
+            dbContext.Genres
+                     .Select(genre => new GenreDto(genre.Id, genre.Name))
+                     .AsNoTracking());
     }
 }
