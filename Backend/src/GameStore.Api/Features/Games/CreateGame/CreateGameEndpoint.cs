3577f0210769e7fbff9d7967fa3f2d7458f51c1d
francescoFH <francescofacca@gmail.com>
2025-09-30 23:27:00 +0100

    Logging - Using ILoggerFactory

--- Backend/src/GameStore.Api/Features/Games/CreateGame/CreateGameEndpoint.cs
+++ Backend/src/GameStore.Api/Features/Games/CreateGame/CreateGameEndpoint.cs
@@ -12,7 +12,8 @@ public static class CreateGameEndpoint
         // POST /games
         app.MapPost("/", async (
             CreateGameDto gameDto,
-            GameStoreContext dbContext) =>
+            GameStoreContext dbContext,
+            ILogger<Program> logger) =>
         {
             var game = new Game
             {
@@ -26,6 +27,11 @@ public static class CreateGameEndpoint
             dbContext.Games.Add(game);
 
             await dbContext.SaveChangesAsync();
+
+            logger.LogInformation(
+                "Created game {GameName} with price {GamePrice}",
+                game.Name,
+                game.Price);
 
             return Results.CreatedAtRoute(
                 EndpointNames.GetGame,
